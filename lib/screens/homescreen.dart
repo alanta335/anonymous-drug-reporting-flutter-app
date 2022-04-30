c7f133a27baaf2750eb341a46c85280afc3bee28
alanta335 <alanta335@gmail.com>
2022-04-30 22:37:10 +0530

    Update homescreen.dart

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -19,37 +19,68 @@ class _HomepageState extends State<Homepage> {
     ChatMessage(messageContent: "ehhhh, doing OK.", messageType: "receiver"),
     ChatMessage(
         messageContent: "Is there any thing wrong?", messageType: "sender"),
+    ChatMessage(messageContent: "Hello, Will", messageType: "receiver"),
+    ChatMessage(messageContent: "How have you been?", messageType: "receiver"),
+    ChatMessage(
+        messageContent: "Hey Kriss, I am doing fine dude. wbu?",
+        messageType: "sender"),
+    ChatMessage(messageContent: "ehhhh, doing OK.", messageType: "receiver"),
+    ChatMessage(
+        messageContent: "Is there any thing wrong?", messageType: "sender"),
+    ChatMessage(messageContent: "Hello, Will", messageType: "receiver"),
+    ChatMessage(messageContent: "How have you been?", messageType: "receiver"),
+    ChatMessage(
+        messageContent: "Hey Kriss, I am doing fine dude. wbu?",
+        messageType: "sender"),
+    ChatMessage(messageContent: "ehhhh, doing OK.", messageType: "receiver"),
+    ChatMessage(
+        messageContent: "Is there any thing wrong?", messageType: "sender"),
   ];
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: messages.length,
-        shrinkWrap: true,
-        padding: EdgeInsets.only(top: 10, bottom: 10),
-        physics: NeverScrollableScrollPhysics(),
-        itemBuilder: (context, index) {
-          return Container(
-            padding: EdgeInsets.only(left: 14, right: 14, top: 10, bottom: 10),
-            child: Align(
-              alignment: (messages[index].messageType == "receiver"
-                  ? Alignment.topLeft
-                  : Alignment.topRight),
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: (messages[index].messageType == "receiver"
-                      ? Colors.grey.shade200
-                      : Colors.blue[200]),
-                ),
-                padding: EdgeInsets.all(16),
-                child: Text(
-                  messages[index].messageContent,
-                  style: TextStyle(fontSize: 15),
-                ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: messages.length,
+                padding: EdgeInsets.only(top: 10, bottom: 10),
+                itemBuilder: (ctx, index) {
+                  return Container(
+                    padding: EdgeInsets.only(
+                        left: 14, right: 14, top: 10, bottom: 10),
+                    child: Align(
+                      alignment: (messages[index].messageType == "receiver"
+                          ? Alignment.topLeft
+                          : Alignment.topRight),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: (messages[index].messageType == "receiver"
+                              ? Colors.grey.shade200
+                              : Colors.blue[200]),
+                        ),
+                        padding: EdgeInsets.all(16),
+                        child: Text(
+                          messages[index].messageContent,
+                          style: TextStyle(fontSize: 15),
+                        ),
+                      ),
+                    ),
+                  );
+                },
               ),
             ),
-          );
-        },
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(),
+                ),
+                ElevatedButton(onPressed: () {}, child: Text('Send')),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
