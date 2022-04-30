910f3af08a4ef47b3db0724947ff4067b5d6f50e
ilmentore72 <s.akhilkrishna@gmail.com>
2022-04-30 22:23:04 +0530

    Chat

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'chatMessageModel.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({Key? key}) : super(key: key);
@@ -9,9 +10,47 @@ class Homepage extends StatefulWidget {
 
 class _HomepageState extends State<Homepage> {
   @override
+  List<ChatMessage> messages = [
+    ChatMessage(messageContent: "Hello, Will", messageType: "receiver"),
+    ChatMessage(messageContent: "How have you been?", messageType: "receiver"),
+    ChatMessage(
+        messageContent: "Hey Kriss, I am doing fine dude. wbu?",
+        messageType: "sender"),
+    ChatMessage(messageContent: "ehhhh, doing OK.", messageType: "receiver"),
+    ChatMessage(
+        messageContent: "Is there any thing wrong?", messageType: "sender"),
+  ];
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(child: Text('Anoop')),
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: messages.length,
+        shrinkWrap: true,
+        padding: EdgeInsets.only(top: 10, bottom: 10),
+        physics: NeverScrollableScrollPhysics(),
+        itemBuilder: (context, index) {
+          return Container(
+            padding: EdgeInsets.only(left: 14, right: 14, top: 10, bottom: 10),
+            child: Align(
+              alignment: (messages[index].messageType == "receiver"
+                  ? Alignment.topLeft
+                  : Alignment.topRight),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: (messages[index].messageType == "receiver"
+                      ? Colors.grey.shade200
+                      : Colors.blue[200]),
+                ),
+                padding: EdgeInsets.all(16),
+                child: Text(
+                  messages[index].messageContent,
+                  style: TextStyle(fontSize: 15),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
