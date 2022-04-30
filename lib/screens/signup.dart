ba5ea849cdc30c2c3a5c93bffd05d1be253ee551
alanta335 <alanta335@gmail.com>
2022-04-30 20:47:54 +0530

    sign up

---
+++ lib/screens/signup.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:report/main.dart';
+
+import 'dart:async';
+
+import 'homescreen.dart';
+
+class Signup extends StatefulWidget {
+  @override
+  _SignupState createState() => _SignupState();
+}
+
+class _SignupState extends State<Signup> {
+  @override
+  final _formkey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Stack(
+        children: [
+          Center(
+              child: GestureDetector(
+            onTap: () {
+              final provider =
+                  Provider.of<GoogleSignInProvider>(context, listen: false);
+              provider.googleLogin();
+              Timer(
+                  Duration(seconds: 10),
+                  () => Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (context) => Homepage())));
+            },
+            child: Container(
+              height: 50,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(50),
+              ),
+              width: width * 0.87,
+              child: Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Row(
+                  children: [
+                    Text(
+                      'Sign up with Google',
+                      style: TextStyle(
+                          fontFamily: 'Poppins', color: Colors.grey.shade700),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          )),
+        ],
+      ),
+    );
+  }
+}
