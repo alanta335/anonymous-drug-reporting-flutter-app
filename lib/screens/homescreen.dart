011ecf0345fc916a727d4923a413b24fd21e9b61
Akhil Krishna S <s.akhilkrishna@gmail.com>
2022-05-01 09:58:11 +0530

    Image Preview set

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,5 +1,6 @@
+import 'dart:convert';
 import 'dart:io';
-
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -15,13 +16,29 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   File? image;
   Future getImage() async {
-    final image = await ImagePicker().pickImage(source: ImageSource.camera);
+    //for the image picking and uploading it to firebase as string?
+
+    final image = await ImagePicker()
+        .pickImage(source: ImageSource.camera, maxHeight: 480, maxWidth: 640);
     if (image == null) return;
     final imageTemp = File(image.path);
     setState(() {
       this.image = imageTemp;
     });
     print(image.path);
+    List<int> imageBytes = await image.readAsBytes();
+    print(imageBytes);
+    String base64Image = base64Encode(imageBytes);
+    FirebaseFirestore.instance
+        .collection('USERS')
+        .doc('${FirebaseAuth.instance.currentUser!.uid}')
+        .collection('message')
+        .doc()
+        .set({
+      'text': base64Image,
+      'type': "image",
+      'time': DateTime.now().toString()
+    });
   }
 
   final ImagePicker _picker = ImagePicker();
@@ -59,14 +76,15 @@ class _HomepageState extends State<Homepage> {
           body: SafeArea(
             child: Column(
               children: [
-                (image != null)
-                    ? Image.file(
-                        image!,
-                        width: 160,
-                        height: 160,
-                        fit: BoxFit.cover,
-                      )
-                    : const SizedBox(),
+                // ##########FOR PREVIEW OF IMAGE
+                // (image != null)
+                //     ? Image.file(
+                //         image!,
+                //         width: 160,
+                //         height: 160,
+                //         fit: BoxFit.cover,
+                //       )
+                //     : const SizedBox(),
                 Expanded(
                   child: ListView(
                     addAutomaticKeepAlives: false,
@@ -79,9 +97,10 @@ class _HomepageState extends State<Homepage> {
                             left: 14, right: 14, top: 10, bottom: 10),
                         child: Align(
                           alignment:
-                              (document.get('type').toString() == "receiver"
-                                  ? Alignment.topLeft
-                                  : Alignment.topRight),
+                              (document.get('type').toString() == "sender" ||
+                                      document.get('type').toString() == "image"
+                                  ? Alignment.topRight
+                                  : Alignment.topLeft),
                           child: Container(
                             decoration: BoxDecoration(
                               borderRadius: BorderRadius.circular(20),
@@ -91,10 +110,18 @@ class _HomepageState extends State<Homepage> {
                                       : Colors.blue[200]),
                             ),
                             padding: EdgeInsets.all(16),
-                            child: Text(
-                              document.get('text'),
-                              style: TextStyle(fontSize: 15),
-                            ),
+                            child: (document.get('type').toString() == "image")
+                                ? Image.memory(
+                                    Base64Decoder().convert(
+                                        document.get('text').toString()),
+                                    width: 140,
+                                    height: 320,
+                                  ) //imgDec(document.get('text').toString())
+                                : Text(
+                                    document.get('text'),
+                                    // ignore: prefer_const_constructors
+                                    style: TextStyle(fontSize: 15),
+                                  ),
                           ),
                         ),
                       );
